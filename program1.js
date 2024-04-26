89b81cb4952b6fef2031e8ba15ff0ce550415d61
Aniket132003 <aniketagrawal132003@gmail.com>
2024-04-26 03:58:41 -0700

    Add existing file

--- program1.js
+++ program1.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
     
+    for (let char of s) {
+        if (brackets[char]) { 
+            stack.push(char);
+        } else { 
+            const lastOpenBracket = stack.pop();
+            if (brackets[lastOpenBracket] !== char) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
